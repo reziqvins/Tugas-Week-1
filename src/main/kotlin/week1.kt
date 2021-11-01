a4474d6689fa1206e6db4d1025825a9844ead48c
reziqvins <nreziq@gmail.com>
2021-11-01 19:13:09 +0700

    tugas ngeprint nama kelas dkk

---
+++ src/main/kotlin/week1.kt
@@ -0,0 +1,9 @@
+ fun main (){
+        val nama = "Muhammad Reziq Darusman"
+        val nim = 20090127
+        val kelas ="2B"
+        val prodi = "Teknik Informatika"
+        println ("Nama $nama, nim $nim, kelas $kelas, prodi $prodi ")
+         val intro ="Nama $nama, nim $nim, kelas $kelas, prodi $prodi"
+         println ("panjang karakternya ada ${intro.length} huruf")
+    }
